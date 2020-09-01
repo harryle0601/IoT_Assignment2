4749fa69951ec0356b5944613b5b536dbf99266c
harryle0601 <harryle0601@gmail.com>
2020-09-01 19:34:07 +0700

    Front end given up, Face Reg need back-end to function

---
+++ Facial_recognition/Check_Auth.py
@@ -0,0 +1,4 @@
+import Facial_Reg as FR
+
+if __name__ == "__main__":
+    FR.Authentication()
--- frontend/frontend/src/components/layout/Header.js
+++ frontend/frontend/src/components/layout/Header.js
@@ -1,36 +1,23 @@
+import React, { Component } from "react";
 
+class Header extends Component {
+  render() {
+    return (
+      <nav>
+        <div class="nav-wrapper">
+          <a href="/" class="brand-logo">
+            Logo
+          </a>
+          <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li>
+              <a href="/login">Login</a>
+            </li>
+          </ul>
+        </div>
 
-import React, {Component} from 'react'
-
-class Header extends Component{
-render(){
-    return(
-
-    <nav>
-    <div class="nav-wrapper">
-      <a href="/" class="brand-logo">Logo</a>
-      <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href="/login">Login</a></li>
-   
-      </ul>
-    </div>
-  </nav>
         
-    //     <nav>
-    //     <ul class="menu">
-    //         <li class="logo"><a href="/">ConCho</a></li>
-    //         <li class="item"><a href="/">Home</a></li>
-    //         <li class="item"><a href="/">About</a></li>
-    //         <li class="item"><a href="/">Services</a></li>
-    //         <li class="item button"><a href="/login">Log In</a></li>
-    //         <li class="item button secondary"><a href="/">Sign Up</a></li>
-    //         <li class="toggle"><span class="bars"></span></li>
-    //     </ul>
-    // </nav>
+      </nav>
     );
+  }
 }
-
-
-
-}
-export default Header
+export default Header;
