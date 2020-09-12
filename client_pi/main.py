26d9685af88cef433b083676024d719265215a58
Limbo2Forma <nam671999hai@gmail.com>
2020-09-12 04:08:11 +0100

    ClientSocket modified

--- client_pi/main.py
+++ client_pi/main.py
@@ -6,9 +6,9 @@ CAR_ID = "0"
 def main_wrapper():
     global CAR_ID
     opt = ''
-    while opt != 1 and opt != 2:
+    while opt != "1" and opt != "2":
         opt = input('Please select usage type:\n1.Customer\n2.Engineer')
-    if opt == 1:
+    if opt == "1":
         command, data = ap.user_authentication()
     else:
         command, data = ap.engineer_authentication()
