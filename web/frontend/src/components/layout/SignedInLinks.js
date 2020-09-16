8b58a480974d61ba5647743d2b0563fe89064609
Limbo2Forma <nam671999hai@gmail.com>
2020-09-16 12:01:36 +0700

    ee

--- web/frontend/src/components/layout/SignedInLinks.js
+++ web/frontend/src/components/layout/SignedInLinks.js
@@ -67,43 +67,6 @@ const SignedInLinks = (props) => {
         <div>
             <Grid container spacing={3}>
                 <Grid item xs={3} style={{ marginTop: "1%", marginRight: '-1%' }}>
-                    <NavLink to={'/cart'}  >
-                        <IconButton>
-                            <Badge badgeContent={props.props.cart ? props.props.cart.length : 0} color="secondary">
-                                <ShoppingCartIcon />
-                            </Badge>
-                        </IconButton>
-                    </NavLink>
-                </Grid>
-                <Grid item xs={3} style={{ marginTop: "1%" }}>
-                    <IconButton onClick={handleClick}>
-                        <Badge badgeContent={props.props.notifications ? props.props.notifications.length : 0} color="secondary">
-                            <Notifications />
-                        </Badge>
-                    </IconButton>
-                    <Menu
-                        id="long-menu"
-                        anchorEl={anchorEl}
-                        keepMounted
-                        open={open}
-                        onClose={handleClose}>
-                        {props.props.notifications ? props.props.notifications.length !== 0 ? props.props.notifications.map((noti, key) =>
-                            noti.link
-                                ? <NavLink to={noti.link}>
-                                    <MenuItem onClick={handleClose} key={key} style={{ maxWidth: '300px', whiteSpace: 'normal', color: 'black' }}>
-                                        <p> {noti.content}</p>
-                                        <p style={{ fontSize: '10px', marginTop: '40px', textAlign: 'right', marginRight: '5px' }}>from {moment(noti.time.toDate()).fromNow()}</p>
-                                    </MenuItem></NavLink>
-                                : <MenuItem onClick={handleClose} key={key} style={{ maxWidth: '300px', whiteSpace: 'normal' }}>
-                                    <p> {noti.content}</p>
-                                    <p style={{ fontSize: '10px', marginTop: '40px', textAlign: 'right', marginRight: '5px' }}>from {moment(noti.time.toDate()).fromNow()}</p>
-                                </MenuItem>
-                        )
-                            : <MenuItem onClick={handleClose}>You have no notification</MenuItem> 
-                            : <MenuItem onClick={handleClose}>You have no notification</MenuItem>}
-                    </Menu>
-                </Grid>
-                <Grid item xs={3}>
                     <UserMenu props={auth} lastContact={props.props.lastContact} currentUser={props.props.currentUser} handleLogOut={handleLogOut} />
                 </Grid>
             </Grid>
