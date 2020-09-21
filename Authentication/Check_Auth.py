e1d51f21c0ba63a474704529cde439b13d4eccdb
Limbo2Forma <nam671999hai@gmail.com>
2020-09-21 19:19:18 +0100

    client side update

--- Authentication/Check_Auth.py
+++ Authentication/Check_Auth.py
@@ -3,8 +3,6 @@
 import authenticate as auth
 
 if __name__ == "__main__":
-    data = auth.take_picture()
-    f = open("dataimg.txt", "a")
-    f.write(data.hex())
-    auth.decode(data)
-
+    
+    data = auth.user_authentication()
+    print(data)
