fa2264a835d03acab585d7a7175ab96ef67066ce
Frozenlemon <50400094+Frozenlemon@users.noreply.github.com>
2020-09-22 03:38:23 +0700

    added comment and minor edit to variables name
    + added comment for all required functions
    * change variables name for clarity

--- client_pi/ClientSocket.py
+++ client_pi/ClientSocket.py
@@ -13,23 +13,35 @@ client.connect(ADDR)
 
 
 def send_message(msg):
+    """
+        Encode the request message to send to master pi.
+        todo: first send a message to notify the size of the actual message then send the message to master pi. Then wait for return message
+    """
     message = msg.encode(FORMAT)
     msg_length = len(message)
     send_length = str(msg_length).encode(FORMAT)
     send_length += b' ' * (HEADER - len(send_length))
     client.send(send_length)
-    print (msg_length)
     client.send(message)
-    receive_message()
+    return_msg = receive_message().split(', ', 1)
+    return return_msg[0], return_msg[1]
 
 def receive_message():
+    """
+        decode the received message from master pi.
+        todo: receive the size of the return message then call recv_all function
+    """
     msg_length = client.recv(HEADER).decode(FORMAT)
     if msg_length:
         msg_length = int(msg_length)
         msg = recv_all(client, msg_length).decode(FORMAT)
-        print(msg)
+        return msg
         
 def recv_all(conn, msg_length):
+    """
+        Continue to receive the TCP packages until no more packages left or until fully receive the return message from server.
+        todo: continue to receive the data from master until the specify length reach or no more package presented. then return the full data
+    """
     data = bytearray()
     while len(data) < msg_length:
         package = conn.recv(msg_length - len(data))
@@ -37,8 +49,3 @@ def recv_all(conn, msg_length):
             return None
         data.extend(package)
     return data
-
-def change_car_id(car_id):
-    global CAR_ID
-    CAR_ID = car_id
-
