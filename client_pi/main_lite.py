04430406c637e7c14f38395213cb5062efee135c
Frozenlemon <50400094+Frozenlemon@users.noreply.github.com>
2020-09-22 01:20:23 +0700

    update main_lite

--- client_pi/main_lite.py
+++ client_pi/main_lite.py
@@ -1,18 +1,18 @@
 import client_pi.ClientSocket as cs
-import getpass
+from getpass import getpass
 
 CAR_ID = 0
 
 def Uname_pass():
     username = input("Username: ")
-    password = getpass.getpass()
+    password = getpass("Password: ")
     return 'UP, ' + username + ', ' + password
 
 def main_wrapper():
     global CAR_ID
     user_cred = Uname_pass()
-    user_cred += str(CAR_ID)
-    cs.send_message(user_cred)
+    user_cred += ', ' + str(CAR_ID)
+    # cs.send_message(user_cred)
 
 while True:
     main_wrapper()
