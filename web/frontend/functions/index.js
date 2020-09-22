3d0a9894c20efec316d429d7494dc75e0e53cbdb
Thanh Quang <43515355+daumaydemoctoca@users.noreply.github.com>
2020-09-22 08:10:16 +0700

    :(((((

--- web/frontend/functions/index.js
+++ web/frontend/functions/index.js
@@ -1,25 +1,19 @@
 const functions = require('firebase-functions');
-// const admin = require('firebase-admin');
-const sendgridemail = require('@sendgrid/mail');
-const MY_SENDGRID_API_KEY = "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"
-sendgridemail.setApiKey(MY_SENDGRID_API_KEY);
+const admin = require('firebase-admin');
+
 
 
 exports.issueNew = functions.firestore
 .document('issues/{issuesId}')
 .onCreate((snap, context) => {
-
   const issue = snap.data();
-  msgbody = {
+ return admin.firestore().collection('mail').add({
     to: "vthanhquang72882@gmail.com",
-    from: 'auto-no-reply@reca.com',
-    templateId: 'd-c7f85eb631714add982b7ef77a07361c',
-    "dynamic_template_data": {
-      name: "Quang",
-      text: "A Car "+issue.Car.Brand+issue.Car.Model+" has been reported for issue of "+issue.Description+" at" +issue.Latitude?issue.Latitude:"10.7296"+", "+issue.Longitude?issue.Longitude:"106.6931"+". Please anrrange come to diagnosis and fix the issue of the car. If there is any help or further instructions needed, please contact reca.admin@reca.com.",
+    message: {
       subject: issue.Sumary,
-    }}
-    return sendgridemail.send(msgbody)
+      html: "Hello Engineers, A Car "+issue.Car.Brand+issue.Car.Model+" has been reported for issue of "+issue.Description+" at" +issue.Latitude?issue.Latitude:"10.7296"+", "+issue.Longitude?issue.Longitude:"106.6931"+". Please anrrange come to diagnosis and fix the issue of the car. If there is any help or further instructions needed, please contact reca.admin@reca.com.",
+    },
+  })
     .then(() => console.log('email sent'))
     .catch(err => console.log(err))
 
